eea07d109bbda4d726485bbccaf97cf41306a9a3
Ryan <ryanvb@hawaii.edu>
2015-12-07 21:40:30 -1000

    415 Project Part 1
    Progress so far

---
+++ ICS415Route.js
@@ -0,0 +1,30 @@
+var Posts = new Meteor.Collection('posts');
+if (Meteor.isClient) {
+    Accounts.ui.config({ passwordSignupFields: 'USERNAME_ONLY' });
+    Template.form.events = {
+        'click #submit': function(event){
+            event.preventDefault();
+            var title = $('#title').val();
+            var body = $('#body').val();
+            Posts.insert({
+                title: title,
+                username: Meteor.user().username,
+                body: body
+            });
+            $('#title, #body').val('');
+        }
+    }
+    Template.posts.helpers({
+        posts: function() {
+            return Posts.find();
+        }
+    })
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+Router.route('/forumpage');
+Router.route('/', {template: 'home'});
